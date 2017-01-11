abd4f1adfdfa057ddd50320b45b158579ca451a7
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-11 11:17:35 +0000

    first commit

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 def index():
     return 'Hello World'
 if __name__ == "__main__":
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+    app.run()
