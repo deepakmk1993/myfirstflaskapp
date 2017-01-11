cd6b7d3aa209e0910de8d958db60d379dffc73bd
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-11 11:45:40 +0000

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return """
+    <!DOCTYPE html>
+    <head>
+       <title>My First Flask Application</title>
+    </head>
+    <body>
+        <h1>My First Flask Application</h1>
+    </body>
+    """
 if __name__ == "__main__":
     app.run()
